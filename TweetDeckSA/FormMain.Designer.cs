b8bf0de0ab7580479addd995bad72181aed99f83
Jiiks <jiiks@windowslive.com>
2015-08-22 16:59:18 +0300

    initial

---
+++ TweetDeckSA/FormMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TweetDeckSA
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.webControl = new Awesomium.Windows.Forms.WebControl(this.components);
+            this.SuspendLayout();
+            // 
+            // webControl
+            // 
+            this.webControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webControl.Location = new System.Drawing.Point(0, 0);
+            this.webControl.Size = new System.Drawing.Size(1152, 553);
+            this.webControl.Source = new System.Uri("https://tweetdeck.twitter.com", System.UriKind.Absolute);
+            this.webControl.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1152, 553);
+            this.Controls.Add(this.webControl);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "TweetDeck - Standalone";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Awesomium.Windows.Forms.WebControl webControl;
+    }
+}
+
---
+++ TweetDeckSA/FormMain.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using Awesomium.Core;
+
+namespace TweetDeckSA
+{
+    public partial class FormMain : Form
+    {
+
+        private const int PrefWidth = 1655;
+        private const int PrefHeight = 800;
+        private readonly String _appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            string sessionFolder = Path.Combine(_appData, "TweetDeckSA");
+
+            Width = PrefWidth;
+            Height = PrefHeight;
+            Debug.Print(sessionFolder);
+            WebSession session = WebCore.CreateWebSession(@sessionFolder, WebPreferences.Default);
+
+            webControl.ShowCreatedWebView += (sender, args) => Process.Start(args.TargetURL.ToString());
+
+        }
+    }
+}
